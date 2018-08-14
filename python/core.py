06c2dab4883b7aec11ccdb6427cf1e6f381579f1
Alexander Serebryakov <alex.serebr@gmail.com>
2018-08-14 14:17:01 +0300

    Implements basic tokens processing

--- python/core.py
+++ python/core.py
@@ -3,6 +3,71 @@ from simplex import Tokenizer
 from simplex import Token
 
 
+# States
+# INIT
+# NOT_BRANCH_SCOPE
+# BRANCH_START
+# IN_BRANCH_SCOPE
+# ALTERNATIVE_BRANCH
+# IN_ALTERNATIVE_BRANCH_SCOPE
+# SCOPE_END
+
+class TokenProcessor:
+    def __init__(self):
+       self.stack = []
+       self.state = 'INIT'
+
+    def handle_token(self, token):
+        print("append {} state {}".format(token, self.state))
+
+        if token.kind == 'BRANCH_START':
+            self.handle_branch_start()
+        elif token.kind == 'BRANCH_ALTERNATIVE':
+            self.handle_branch_alternative()
+        elif token.kind == 'SCOPE_START':
+            self.handle_scope_start()
+        elif token.kind == 'SCOPE_END':
+            self.handle_scope_end()
+
+        self.stack.append(token)
+
+    def handle_branch_start(self):
+        previous_state = self.state
+        self.state = 'BRANCH_START'
+
+        if previous_state != 'SCOPE_END':
+            return
+
+        while len(self.stack) > 0 and self.stack[-1].kind != 'BRANCH_START':
+            self.stack.pop()
+
+        # Removes the BRANCH_START when scope is ended
+        if len(self.stack) > 0:
+            self.stack.pop()
+
+
+    def handle_branch_alternative(self):
+        previous_state = self.state
+        self.state = 'ALTERNATIVE_BRANCH'
+
+        if previous_state != 'SCOPE_END':
+            return
+
+        while len(self.stack) > 0 and self.stack[-1].kind != 'BRANCH_START' and self.stack[-1].kind != 'ALTERNATIVE_BRANCH':
+            self.stack.pop()
+
+    def handle_scope_start(self):
+        if self.state == 'INIT':
+            self.state = 'NOT_BRANCH_SCOPE'
+        elif self.state == 'BRANCH_START':
+            self.state = 'IN_BRANCH_SCOPE'
+        elif self.state == 'ALTERNATIVE_BRANCH':
+            self.state = 'IN_ALTERNATIVE_BRANCH_SCOPE'
+
+    def handle_scope_end(self):
+        self.state = 'SCOPE_END'
+
+
 def handler_generic(match_object, kind, value, keywords, state):
     column = match_object.start() - state['line_start']
     return Token(kind, value, state['line_num'], column)
@@ -50,20 +115,17 @@ def core_main():
 #                                                     |
 #                                         Decision to remove tokens is here
 def estimate_stack(tokens):
-    stack = []
+    processor = TokenProcessor()
 
     for token in tokens:
-        if token.kind == 'BRANCH_START' or token.kind == 'SCOPE_START' or token.kind == 'BRANCH_ALTERNATIVE':
-            stack.append(token)
-        elif token.kind == 'SCOPE_END':
-            stack.pop()
-            while stack[-1].kind == 'BRANCH_START' or stack[-1].kind == 'BRANCH_ALTERNATIVE':
-                stack.pop()
+        processor.handle_token(token)
+
+    print(processor.stack)
 
     vim.command('set errorformat=%f:%l:%m')
     vim.command('lexpr [' +
         ','.join(["'{}:{}:{}'".format(vim.current.buffer.name, token.line, vim.current.buffer[token.line - 1])
-        for token in stack
+        for token in processor.stack
         if token.kind != 'SCOPE_START']) +
         ']')
 
